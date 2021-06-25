9d84019f117d15f7b94df484e91cf7c6aa2fce2b
Nick Landon <nicklandon.co@gmail.com>
2021-06-24 20:12:12 -0700

    Create Trivia Framework and Display question/answers

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 //1. Get Trivia Game to display questions
     //When play button is pressed, run "createTrivia" function
+    //DONE
 //2. Get answers to display correctly
+    //DONE
 //3. Allow answer to be selected
     //Create variable for answer
+    //DONE
 //4. Get "submit" to work
     //Grab selected answer variable
     //Compare it to answer
+    //NEED HELP
 //5. Get "right/wrong" to work/display
     //If selection === answer, display right
     //Else if selection !== answer, display wrong
@@ -16,16 +20,46 @@
 
 console.log('sanity check');
 
-async function getWeatherData(e){
-e.preventDefault();
+async function createTrivia(e){
+    e.preventDefault();
 
-const apiKey = 'f31e429a5567d1c28d46897699a0ab05';
-const userInput = $('input[type="text"]').val();
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}&units=imperial`;
+const url = `https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple`;
 
 
 const response = await fetch(url);
 const data = await response.json();
 console.log(data);
 
-}
+$('#question').html(data.results[0].question);
+$('#answer1').html(data.results[0].incorrect_answers[0]);
+$('#answer2').append(data.results[0].correct_answer);
+$('#answer3').append(data.results[0].incorrect_answers[1]);
+$('#answer4').append(data.results[0].incorrect_answers[2]);
+
+// function checkAnswer(event){
+// if (event.target.html === data.results[0].correct_answer){
+//             console.log('correct!');
+//         };
+//     };
+
+// $('.option').on('click', checkAnswer);
+
+};
+
+async function checkAnswer(event){
+    const url = `https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple`;
+    
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    
+    if (event.target == data.results[0].correct_answer){
+        console.log('correct!');
+    } else {
+        console.log('error');
+    }
+};
+
+$('#play-now').on('click', createTrivia);
+$('.option').on('click', checkAnswer);
