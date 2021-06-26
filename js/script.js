bef5e044c0904532d8645f7b8133c18407d28bdd
Nick Landon <nicklandon.co@gmail.com>
2021-06-26 15:18:32 -0700

    Answers console as correct/wrong

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@
 //6. Select next question
     //When next question is clicked, call trivia function again
 //7. Get "new game" to work
+//8. Get landing page to disappear and lead to page
 
 
 console.log('sanity check');
@@ -32,34 +33,20 @@ console.log(data);
 
 $('#question').html(data.results[0].question);
 $('#answer1').html(data.results[0].incorrect_answers[0]);
-$('#answer2').append(data.results[0].correct_answer);
-$('#answer3').append(data.results[0].incorrect_answers[1]);
-$('#answer4').append(data.results[0].incorrect_answers[2]);
+$('#answer2').html(data.results[0].correct_answer);
+$('#answer3').html(data.results[0].incorrect_answers[1]);
+$('#answer4').html(data.results[0].incorrect_answers[2]);
 
-// function checkAnswer(event){
-// if (event.target.html === data.results[0].correct_answer){
-//             console.log('correct!');
-//         };
-//     };
 
-// $('.option').on('click', checkAnswer);
-
-};
-
-async function checkAnswer(event){
-    const url = `https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple`;
-    
-    
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log(data);
-    
-    if (event.target == data.results[0].correct_answer){
+$('.option').on('click', function(e){
+    if (e.target.innerHTML === data.results[0].correct_answer){
         console.log('correct!');
     } else {
-        console.log('error');
+        console.log('wrong');
     }
+});
+
+
 };
 
 $('#play-now').on('click', createTrivia);
-$('.option').on('click', checkAnswer);
